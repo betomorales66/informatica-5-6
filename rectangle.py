b096f5105b3246feb80e417c8cbf064680a54aee
betomorales66 <beto001ms@gmail.com>
2026-08-17 19:39:55 +0000

    simon

---
+++ rectangle.py
@@ -0,0 +1,20 @@
+def main():
+    width = int(input("enter the width of the rectagle: "))
+    print("o" * width)
+    print("o" * width)
+    print("o" * width)
+    print("o" * width)
+    print("o" * width)
+
+    a = width * 5
+    p = width * 2 + width * 2
+
+    print(
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
