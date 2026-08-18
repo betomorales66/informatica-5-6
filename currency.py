a5a6a0ce446d98642a379960acef851c5ba4cf89
betomorales66 <beto001ms@gmail.com>
2026-08-18 16:40:40 +0000

    dolar

---
+++ currency.py
@@ -0,0 +1,25 @@
+def main():
+
+    p = int(input("What do you have left in pesos?"))
+    s = int(input("What do you have left in soles:? "))
+    r = int(input("What do you have left in reais? "))
+
+    mxn = (p * 0.0054) + (s * 0.20) + (r * 0.31)
+    usd = (p * 0.00032) + (s * 0.30) + (r * 0.19)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
