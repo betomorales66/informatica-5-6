5b178f8ab05c7357f8915cd812c4707b853d36cd
betomorales66 <beto001ms@gmail.com>
2026-08-24 19:35:44 +0000

    sisoy

---
+++ password.py
@@ -0,0 +1,24 @@
+def main():
+
+    password = "Soyelcachorromasguapo"
+    attempt = (input("enter your password:"))
+
+    if attempt == password:
+        print("good boy")
+
+    print("bye bye")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
