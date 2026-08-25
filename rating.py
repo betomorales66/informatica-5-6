fb6cabda319852c8ac45232f4dc54a541ae976a9
betomorales66 <beto001ms@gmail.com>
2026-08-25 16:31:46 +0000

    trabajaso bn perro

---
+++ rating.py
@@ -0,0 +1,37 @@
+def main():
+
+    print("Tortas chumas")
+
+    rating = int(input("rate our restaurant from 0 to 5: "))
+
+    if rating > 4.5:
+        print("Perfection")
+    elif rating > 4:
+        print("Excellent")
+    elif rating > 3:
+        print("Good")
+    elif rating > 2:
+        print("Fair")
+    else:
+        print("wacala🤢")
+
+    print("thank you for your rating!! 🙏")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
---
+++ worldcup.py
@@ -0,0 +1,21 @@
+def main():
+
+    spain = int(input("spain goals: "))
+    argentina = int(input("argentina goals: "))
+
+    if spain > argentina:
+        print("spain is the winner")
+    elif argentina > spain:
+        print("argentina is the winner")
+    else:
+        print("it is a tie. ")
+
+    print("GG´s me fui. ")
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
