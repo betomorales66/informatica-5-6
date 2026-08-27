700eb0ef7ef73fe33954a7236bb4107b28d2eb0f
betomorales66 <beto001ms@gmail.com>
2026-08-27 16:38:22 +0000

    examen parte 1

---
+++ atmosphere.py
@@ -0,0 +1,57 @@
+def main():
+
+    layer = input("descent atmosphere layer:")
+
+    if layer == "exosphere":
+        print("your altittude level will be between 700 and 10,000 km")
+
+    elif layer == "thermosphere":
+            print("your altittude level will be between 85 and 700 km km")
+
+    elif layer == "mesosphere":
+            print("your altittude level will be between 50 and 85 km")
+
+    elif layer == "stratosphere":
+            print("your altittude level will be between 12 and 50 km")
+
+    elif layer == "troposphere":
+            print("your altittude level will be between 0 and 12 km")
+
+    else:
+          print("ERR0R 😡")
+
+    altittude = float(input("Enter exact altittude:"))
+
+    if altittude >
+            print("")
+
+        elif altittude >
+            print("")
+
+        elif altittude >
+            print("")
+
+        elif altittude >
+            print("")
+
+        elif altittude >
+            print("")
+
+        else:
+            print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
