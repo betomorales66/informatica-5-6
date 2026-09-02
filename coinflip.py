b2bdc6b0efa2782d6d6a2fabe191e4baa19b7ad0
betomorales66 <beto001ms@gmail.com>
2026-09-02 20:14:39 +0000

    guardar

---
+++ coinflip.py
@@ -0,0 +1,23 @@
+import random
+
+def main():
+
+    coin = ["heads", "tails"]
+    attempts = 3
+    while attempts > 0:
+        flip = random.choice(coin)
+        guess = input("heads or tails? ").strip().lower()
+
+        print("the coin landed on", flip)
+
+        if guess == flip:
+            print("you won!")
+            break
+        else:
+            print("GG´s")
+            attempts -= 1
+            print("attempts left:", attempts)
+
+
+if __name__=="__main__":
+    main()
