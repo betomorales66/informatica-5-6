29cb51b895981056b0392cb27b80302f2bca7ff3
betomorales66 <beto001ms@gmail.com>
2026-09-03 17:28:36 +0000

    simonpapu

---
+++ guess-number.py
@@ -0,0 +1,33 @@
+import random
+
+def main():
+
+    number = random.randint(1, 100)
+    answer = number
+
+    name = input("hello what is your name? ")
+
+    print("hello", name, "!" )
+
+    print("Well", name, "I am thinking of a number between 1 and 100.")
+    answer = int(input("Take a guess."))
+
+    while answer != number
+        if answer > number
+        print("your guess is too high")
+        answer = int(input("Take a guess."))
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
