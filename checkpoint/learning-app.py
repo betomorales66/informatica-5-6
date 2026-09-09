b6b3cc798bb324ef9a9559790ea96f743ef10d18
betomorales66 <beto001ms@gmail.com>
2026-09-09 20:24:18 +0000

    si

---
+++ checkpoint/learning-app.py
@@ -0,0 +1,27 @@
+import random
+def main():
+
+    print("rmathilingo")
+    num1 = random.randint(10, 99)
+    num2 = random.randint(10, 99)
+    num3 = num1 + num2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
