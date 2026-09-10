d4f6deb7af44b2a2eb3da4dd6ccaf283fe3f6465
betomorales66 <beto001ms@gmail.com>
2026-09-10 20:20:38 +0000

    si

---
+++ list-reference.py
@@ -0,0 +1,23 @@
+def main():
+
+    numbers = [1, 4, 5, 7, 9, 3]
+    numbers.sort()
+    print(numbers)
+    items = ["Lettuce", "Tomato", "Bread", "Jam", "Mayonaise"]
+    items.sort()
+    items.sort(key=len)
+    items.sort(reverse= true)
+
+
+    print(items)
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
