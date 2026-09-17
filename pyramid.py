6371b49b0476b5b23fc91aa49469712f191c801a
betomorales66 <beto001ms@gmail.com>
2026-09-17 20:13:16 +0000

    si

---
+++ pyramid.py
@@ -0,0 +1,29 @@
+def main():
+
+    height = int(input("height: "))
+
+    for i in range (height):
+        print("😈 " *(i + 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
