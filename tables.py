62fd72611dfd0615d509f3e057642fed68e43e5d
betomorales66 <beto001ms@gmail.com>
2026-09-23 20:20:19 +0000

    si

---
+++ tables.py
@@ -0,0 +1,45 @@
+def main():
+
+    while True:
+        number = int(intput("Enter a number (1-10): "))
+
+
+    if number == "exit":
+        print("bye bye little boy")
+        break
+
+    elif number >= 1:
+        print("try again pls")
+
+    elif number <= 10:
+        print("try again pls")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
